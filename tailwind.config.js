cc40545adbd3376fc6110ffdf08c084619ab95a2
ericholdridge <eholdridge2@gmail.com>
2020-01-07 22:29:11 -0600

    Refactored nav & hero section for responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// Example `tailwind.config.js` file
+module.exports = {
+    important: true,
+    theme: {
+        // screens: {
+        //     'xxl': '1400px',
+        //     // => @media (min-width: 1400px) { ... }
+        // },
+        container: {
+            padding: '0 1rem',
+        },
+        fontFamily: {
+
+        },
+        extend: {
+            colors: {
+            green: '#76aa6f',
+            yellow: '#f9e95e',
+            navBdrColor: 'rgba(255, 255, 255, 0.23)',
+            },
+            margin: {
+            },
+            height: {
+                half: '50%',
+            }
+        }
+    },
+    variants: {
+
+    }
+  }
