2986774f24e787ddc2ac7524ea92507ce203020b
ericholdridge <eholdridge2@gmail.com>
2020-01-14 22:13:20 -0600

    Added carousel slider to hero page

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-console.log("WORKING");
+let slideImages = document.querySelectorAll('.slide');
+console.log(slideImages);
+let arrowLeft = document.querySelector('.prev');
+let arrowRight = document.querySelector('.next');
+let current = 0;
+
+// Clear all images
+function reset() {
+    for(let i = 0; i < slideImages.length; i++) {
+        slideImages[i].style.display = "none";
+    }
+}
+
+// Init the slider
+function startSlide() {
+    reset();
+    slideImages[0].style.display = "block";
+}
+
+// Show previous
+function slideLeft() {
+    reset();
+    slideImages[current - 1].style.display = "block";
+    current = current - 1;
+}
+
+// Show next
+function slideRight() {
+    reset();
+    slideImages[current + 1].style.display = "block";
+    current++;
+}
+
+arrowLeft.addEventListener('click', function(e) {
+    e.preventDefault();
+    if(current === 0) {
+        current = slideImages.length;
+    }
+    slideLeft();
+});
+
+arrowRight.addEventListener('click', function(e) {
+    e.preventDefault();
+    if(current === slideImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
+
+
+
+
+
+
+
+
+
+
+
+
